31c391f945a73aa05a98cc28da3edf022bba5ee5
Florian <florian.martin63000@gmail.com>
2021-06-09 21:50:56 +0200

    Feat: finished demo of collectMultiMap operator

--- src/main/java/com/florian935/basicsfluxmono/transform/CollectMultiMap.java
+++ src/main/java/com/florian935/basicsfluxmono/transform/CollectMultiMap.java
@@ -4,6 +4,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CollectMultiMap {
@@ -11,6 +12,20 @@ public class CollectMultiMap {
     public void collectMultiMap() {
         final Mono<Map<String, Collection<Integer>>> stream = Flux.just(1, 2, 3, 4, 5, 6)
                 .collectMultimap(value -> value % 2 == 0 ? "Pair" : "Impair");
+
+        stream.subscribe(System.out::println);
+    }
+
+    public void collectMultiMapValueExtractor() {
+        final Mono<Map<String, Collection<Integer>>> stream = Flux.just(1, 2, 3, 4, 5, 6)
+                .collectMultimap(value -> value % 2 == 0 ? "Pair" : "Impair", value -> value * 10);
+
+        stream.subscribe(System.out::println);
+    }
+
+    public void collectMultiMapWithSupplier() {
+        final Mono<Map<String, Collection<Integer>>> stream = Flux.just(1, 2, 3, 4, 5, 6)
+                .collectMultimap(value -> value % 2 == 0 ? "Impair" : "Pair", value -> value * 10, HashMap::new);
 
         stream.subscribe(System.out::println);
     }
